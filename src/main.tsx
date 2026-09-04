33afa7b76e80c220bf74bce0cd90d8d30304eacd
raghav3907-svg <raghav3907@gmail.com>
2026-09-04 21:41:02 +0530

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/attendence-tracker/">
       <ThemeProvider>
         <AuthProvider>
           <App />
